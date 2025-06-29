4e481be88a18ef5f3dd51d6e6fcbaa9b8516ddf6
AgrimaPT <agrimapt1999@gmail.com>
2025-06-29 14:25:35 +0530

    urls

--- spinwheelapp/urls.py
+++ spinwheelapp/urls.py
@@ -3,14 +3,15 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('shop/update/', views.update_shop_profile, name='update_shop_profile'),
     path('offers/', views.offer_list, name='offer_list'),
     path('offers/add/', views.add_offer, name='add_offer'),
     path('offers/delete/<int:offer_id>/', views.delete_offer, name='delete_offer'),
     # path('', views.entry_form, name='entry_form'),         # Form page
-    path('spin/<str:shop_code>/', views.qr_entry_form, name='qr_entry_form'),
+    #path('spin/<str:shop_code>/', views.qr_entry_form, name='qr_entry_form'),
+    path('<str:shop_code>/', views.qr_entry_form, name='qr_entry_form'),
     path('spin/', views.spin_page, name='spin_page'),       # Separate spin page
     path('offers/<int:offer_id>/edit/', views.edit_offer, name='edit_offer'),
     path('settings/', views.settings_view, name='settings'),
